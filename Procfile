011f2753c1c24db2190e35c658c00861a7667f4a
Fersandi Pratama <fersandi_pratama@rocketmail.com>
2017-09-23 02:02:37 +0700

    Set Procfile

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: python mamin.py
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
@@ -5,3 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 def main():
 	return "Bot is running"
+
+port = os.getenv('VCAP_APP_PORT', '5000')
+if __name__ == "__main__":
+	app.run(host='0.0.0.0', port=int(port))
