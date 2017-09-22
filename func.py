b2d3d6f04deab8bf8c4d0b8f62f6c0179d206958
Fersandi Pratama <fersandi_pratama@rocketmail.com>
2017-09-23 03:24:28 +0700

    Add scheduler and redis handler

---
+++ func.py
@@ -0,0 +1,25 @@
+import redis
+
+REDIS_HOST = "sl-aus-syd-1-portal.3.dblayer.com"
+REDIS_PORT = 17556
+REDIS_PASSWORD = "BWJPNBCYZAZCAVPD"
+
+def login_redis():
+	return redis.Redis(host=REDIS_HOST, port=REDIS_PORT, password=REDIS_PASSWORD)
+
+def scrap():
+	r = login_redis()
+
+	val = r.get("test")
+	if (val is None):
+		r.set("test", 0)
+	else:
+		r.set("test", val+1)
+
+def test():
+	r = login_redis()
+	val = r.get("test")
+
+	if val is None:
+		val = "None"
+	return str(val)
--- main.py
+++ main.py
@@ -1,20 +1,21 @@
 import os
+import func
 from flask import Flask
 from flask_apscheduler import APScheduler
 
 
 class Config(object):
-    JOBS = [
-        {
-            'id': 'generate-page',
-            'func': 'func:scrap',
-            'args': (1, 2),
-            'trigger': 'interval',
-            'minues': 15
-        }
-    ]
+	JOBS = [
+		{
+			'id': 'generate-page',
+			'func': 'func:scrap',
+			'args': (1, 2),
+			'trigger': 'interval',
+			'minutes': 1
+		}
+	]
 
-    SCHEDULER_API_ENABLED = True
+	SCHEDULER_API_ENABLED = True
 
 app = Flask(__name__)
 
@@ -22,14 +23,18 @@ app = Flask(__name__)
 def main():
 	return "Bot is running"
 
+@app.route('/test')
+def test():
+	return func.test()
+
 port = os.getenv('VCAP_APP_PORT', '5000')
 if __name__ == "__main__":
 	app.config.from_object(Config())
 
-    scheduler = APScheduler()
-    # it is also possible to enable the API directly
-    # scheduler.api_enabled = True
-    scheduler.init_app(app)
-    scheduler.start()
-    
+	scheduler = APScheduler()
+	# it is also possible to enable the API directly
+	# scheduler.api_enabled = True
+	scheduler.init_app(app)
+	scheduler.start()
+	
 	app.run(host='0.0.0.0', port=int(port))
